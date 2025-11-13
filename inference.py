5fac92ad9aa89c608f39f6af4c764a8df82de35f
roshankaranth <roshankaranth@gmail.com>
2025-11-13 22:35:28 +0530

    Generated output with 10M param model

---
+++ inference.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from timeit import default_timer as timer
+
+BATCH_SIZE = 64
+BLOCK_SIZE = 256
+MAX_ITERS = 5000
+EVAL_INTERVAL = 500
+LEARNING_RATE = 3e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+EVAL_ITERS = 200
+N_EMBED = 384
+N_LAYER = 6
+N_HEAD = 6
+DROPOUT = 0.2
+MODEL_PATH = "model_v1"
+FILE_PATH = "output.txt"
+
+torch.manual_seed(1337)
+
+with open("input.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: "".join([itos[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+
+def get_batch(split):
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - BLOCK_SIZE, (BATCH_SIZE,))
+    x = torch.stack([data[i : i + BLOCK_SIZE] for i in ix])
+    y = torch.stack([data[i + 1 : i + BLOCK_SIZE + 1] for i in ix])
+    x, y = x.to(DEVICE), y.to(DEVICE)
+    return x, y
+
+
+class head(nn.Module):
+    """one head of self attention"""
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(N_EMBED, head_size, bias=False)
+        self.query = nn.Linear(N_EMBED, head_size, bias=False)
+        self.value = nn.Linear(N_EMBED, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)))
+
+        self.dropout = nn.Dropout(DROPOUT)
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)  # (B,T,head_size)
+        q = self.query(x)  # (B,T,head_size)
+
+        wei = q @ k.transpose(-2, -1) * C**-0.5
+        wei = torch.masked_fill(wei, self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        v = self.value(x)
+        return wei @ v
+
+
+class MultipleSelfAttentionHead(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(N_EMBED, N_EMBED)
+        self.dropout = nn.Dropout(DROPOUT)
+
+    def forward(self, x):
+        out = self.proj(torch.cat([h(x) for h in self.heads], dim=-1))
+        return self.dropout(out)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, 4 * n_embed),
+            nn.ReLU(),
+            nn.Linear(4 * n_embed, n_embed),
+            nn.Dropout(DROPOUT),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(self, n_embed, n_head):
+        super().__init__()
+        head_size = n_embed // n_head
+        self.sa = MultipleSelfAttentionHead(n_head, head_size)
+        self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(N_EMBED)
+        self.ln2 = nn.LayerNorm(N_EMBED)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, N_EMBED)
+        self.position_embedding_table = nn.Embedding(BLOCK_SIZE, N_EMBED)
+        self.block = nn.Sequential(
+            *[Block(N_EMBED, n_head=N_HEAD) for _ in range(N_LAYER)]
+        )
+        self.lm_head = nn.Linear(N_EMBED, vocab_size)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=DEVICE))
+        x = tok_emb + pos_emb
+        x = self.block(x)
+        logits = self.lm_head(x)
+
+        if targets is not None:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+        else:
+            loss = None
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -BLOCK_SIZE:]
+
+            logits, loss = self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+
+loaded_model = BigramLanguageModel()
+loaded_model.load_state_dict(torch.load(MODEL_PATH, weights_only=True, map_location=DEVICE))
+loaded_model.to(DEVICE)
+
+context = torch.zeros((1,1), dtype = torch.long, device=DEVICE)
+
+time_start = timer()
+
+model_output = decode(loaded_model.generate(context, max_new_tokens=10000)[0].tolist())
+
+time_end = timer()
+
+with open(FILE_PATH, 'w') as file:
+    file.write(model_output)
+
+print(f"Time for generating response : {time_end - time_start} secs")
+
+
+
+
+
