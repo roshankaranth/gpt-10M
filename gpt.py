4c6889c1cf336d5944281a6e5ce8dd6f8c7171ed
roshankaranth <roshankaranth@gmail.com>
2025-12-05 13:03:33 +0530

    Added KV cache

--- gpt.py
+++ gpt.py
@@ -14,13 +14,14 @@ LEARNING_RATE = 3e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 EVAL_ITERS = 200
 N_EMBED = 384 #Embedding dimension
-N_LAYER = 6 
+N_LAYER = 6 #Number of decoder blocks
 N_HEAD = 6  #Number of Attention heads
 DROPOUT = 0.2
 PATH = "model_v2"
 VOCAB_SIZE = 256 #Vocab size of tokenizer
 PATTERN = r"[A-Za-z]+(?:'[A-Za-z]+)?|[A-Za-z']+|[0-9]+|[^\sA-Za-z0-9]"
-
+#The patten at this point does not really effect the outcome of the tokenizer as the tokenizer has a vocab if 256,
+#i.e esentially boils down to byte level encoding.
 torch.manual_seed(1337)
 
 with open("input.txt", "r", encoding="utf-8") as f:
@@ -54,11 +55,11 @@ def precompute_rope_embeddings(head_dim, max_seq_len, base=10000):
     
     return emb
 
-def apply_rotary_pos_emb(x, rope_emb):
+def apply_rotary_pos_emb(x, rope_emb, offset = 0):
 
     B, nh, T, hs = x.shape
-    seq_len = T
-    rope_emb_sliced = rope_emb[:seq_len, :]
+
+    rope_emb_sliced = rope_emb[offset: offset + T, :]
 
     emb = rope_emb_sliced.unsqueeze(0).unsqueeze(0)
 
@@ -75,12 +76,7 @@ class MultipleHeadSelfAttention(nn.Module):
     def __init__(self, num_heads):
         super().__init__()
         self.s_attn = nn.Linear(N_EMBED, 3 * N_EMBED, bias=False) 
-        self.register_buffer(
-            "tril",
-            torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)).view(
-                1, 1, BLOCK_SIZE, BLOCK_SIZE
-            ),
-        )
+        self.register_buffer("tril", torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)).view( 1, 1, BLOCK_SIZE, BLOCK_SIZE))
         self.n_head = num_heads
         self.proj = nn.Linear(N_EMBED, N_EMBED, bias=False)
         self.attn_dropout = nn.Dropout(DROPOUT)
@@ -88,18 +84,35 @@ class MultipleHeadSelfAttention(nn.Module):
         head_dim = N_EMBED // num_heads
         self.register_buffer("rope_embeds", precompute_rope_embeddings(head_dim, BLOCK_SIZE),persistent=False)
 
-    def forward(self, x):
+    def forward(self, x, past_kv = None):
         B, T, C = x.shape #BATCH_SIZE, BLOCK_SIZE, N_EMBED | (during generation (1, 1, N_EMBED))
 
         q, k, v = self.s_attn(x).split(N_EMBED, dim=2) #(B,T,C) * (C, 3*C) -> (B,T,3*C) -> (B,T,C) , (B,T,C) , (B,T,C) | (during generation, (1,1,C) * (C, 3*C) -> (1,1,3*C) -> (1,1,C), (1,1,C), (1,1,C))
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(
-            1, 2
-        )  # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
 
-        q = apply_rotary_pos_emb(q, self.rope_embeds)
-        k = apply_rotary_pos_emb(k, self.rope_embeds)
+        if past_kv is None:
+            past_len = 0
+        else:
+            past_k, past_v = past_kv
+            past_len = past_k.shape[-2]
+
+        max_offset = BLOCK_SIZE - T
+        if past_len > max_offset:
+            past_len = max_offset
+
+        q = apply_rotary_pos_emb(q, self.rope_embeds, offset=past_len)
+        k = apply_rotary_pos_emb(k, self.rope_embeds, offset=past_len)
+
+        if past_kv is not None:
+            k = torch.cat((past_k, k), dim=-2)
+            v = torch.cat((past_v, v), dim=-2)
+
+            if k.shape[-2] > BLOCK_SIZE:
+                k = k[:, :, -BLOCK_SIZE:, :]
+                v = v[:, :, -BLOCK_SIZE:, :]
+
 
         T_k = k.shape[-2]
 
@@ -113,7 +126,7 @@ class MultipleHeadSelfAttention(nn.Module):
 
         out = self.resi_dropout(self.proj(out))
 
-        return out
+        return out, (k,v)
 
 
 class FeedForward(nn.Module):
@@ -138,10 +151,11 @@ class Block(nn.Module):
         self.ln1 = nn.LayerNorm(N_EMBED)
         self.ln2 = nn.LayerNorm(N_EMBED)
 
-    def forward(self, x):
-        x = x + self.sa(self.ln1(x)) #residual path + layer-norm before attention block
+    def forward(self, x, past_kv = None):
+        attn_out, past_kv = self.sa(self.ln1(x), past_kv) #residual path + layer-norm before attention block
+        x = x + attn_out
         x = x + self.ffwd(self.ln2(x)) #residual path + layer norm before feed forward network
-        return x    
+        return x,past_kv    
 
 
 class GPT(nn.Module):
@@ -149,16 +163,24 @@ class GPT(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(VOCAB_SIZE, N_EMBED) #Each unique token will have an embedding of size N_EMBED
         # self.position_embedding_table = nn.Embedding(BLOCK_SIZE, N_EMBED) #Similarly each position in a block, will have an embedding vector of size N_EMBED 
-        self.block = nn.Sequential(*[Block(n_head=N_HEAD) for _ in range(N_LAYER)]) #* is used to convert list of block to positional arguments.
+        # self.block = nn.Sequential(*[Block(n_head=N_HEAD) for _ in range(N_LAYER)]) #* is used to convert list of block to positional arguments.
+        self.blocks = nn.ModuleList([Block(n_head=N_HEAD) for _ in range(N_LAYER)])
         self.lm_head = nn.Linear(N_EMBED, VOCAB_SIZE) #(BLOCK_SIZE * N_EMBED) * (N_EMBED * VOCAB_SIZE)
 
-    def forward(self, idx, targets=None):
+    def forward(self, idx, targets=None, kv_cache = None):
         B, T = idx.shape #BATCH_SIZE, BLOCK_SIZE. (inputs)
 
         tok_emb = self.token_embedding_table(idx) #(B,T,C) where C is N_EMBED
         # pos_emb = self.position_embedding_table(torch.arange(T, device=DEVICE)) #(T,C) matrix
         x = tok_emb#(B,T,C) + (T,C) (broadcasting happens here)
-        x = self.block(x) #Through Transformer (B, T, C) -> (B, T, C)
+        #x = self.block(x) #Through Transformer (B, T, C) -> (B, T, C)
+        if kv_cache is None:
+            for block in self.blocks:
+                x,_ = block(x)
+        else:
+            for i, block in enumerate(self.blocks):
+                x, kv_cache[i] = block(x,kv_cache[i])
+        
         logits = self.lm_head(x) #Language Model head (B, T, C) -> (B, T, VOCAB_SIZE)
 
         if targets is not None:
@@ -169,13 +191,18 @@ class GPT(nn.Module):
         else:
             loss = None
 
-        return logits, loss
+        return logits, loss, kv_cache
 
     def generate(self, idx, max_new_tokens):
-        for _ in range(max_new_tokens):
-            idx_cond = idx[:, -BLOCK_SIZE:]
+        
+        kv_cache = [None for _ in range(N_LAYER)]
 
-            logits, _ = self(idx_cond)
+        logits,_,kv_cache = self(idx, kv_cache = kv_cache) #prefilling the cache
+        for _ in range(max_new_tokens):
+
+            idx_cond = idx[:, -1:]
+
+            logits,_,kv_cache = self(idx_cond, kv_cache = kv_cache)
             logits = logits[:, -1, :]
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
@@ -184,7 +211,7 @@ class GPT(nn.Module):
 
 
 model = GPT()
-m = model.to(DEVICE)
+model = model.to(DEVICE)
 optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
 
 
@@ -196,7 +223,7 @@ def estimate_loss():
         losses = torch.zeros(EVAL_ITERS)
         for k in range(EVAL_ITERS):
             X, Y = get_batch(split)
-            logits, loss = model(X, Y)
+            logits, loss, _ = model(X, Y)
             losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
@@ -214,7 +241,7 @@ for iter in range(MAX_ITERS):
 
     xb, yb = get_batch("train")
 
-    logits, loss = m(xb, yb)
+    logits, loss, _ = model(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
@@ -223,7 +250,7 @@ time_end = timer()
 
 print(f"Time to train model : {time_end - time_start} seconds")
 
-torch.save(m.state_dict(), PATH)
+torch.save(model.state_dict(), PATH)
 
 context = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
-print(tokenizer.decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(tokenizer.decode(model.generate(context, max_new_tokens=500)[0].tolist()))
--- inference.py
+++ inference.py
@@ -28,10 +28,6 @@ print(f"RegexTokenizer training complete!")
 
 data = torch.tensor(tokenizer.encode(text), dtype=torch.long)
 
-n = int(0.9 * len(data)) #size of training set
-train_data = data[:n]
-val_data = data[n:] #validation set
-
 def precompute_rope_embeddings(head_dim, max_seq_len, base=10000):
 
     inv_freq = 1.0/(base ** (torch.arange(0, head_dim, 2).float() / head_dim))
@@ -41,11 +37,11 @@ def precompute_rope_embeddings(head_dim, max_seq_len, base=10000):
     
     return emb
 
-def apply_rotary_pos_emb(x, rope_emb):
+def apply_rotary_pos_emb(x, rope_emb, offset = 0):
 
     B, nh, T, hs = x.shape
-    seq_len = T
-    rope_emb_sliced = rope_emb[:seq_len, :]
+
+    rope_emb_sliced = rope_emb[offset: offset + T, :]
 
     emb = rope_emb_sliced.unsqueeze(0).unsqueeze(0)
 
@@ -62,12 +58,7 @@ class MultipleHeadSelfAttention(nn.Module):
     def __init__(self, num_heads):
         super().__init__()
         self.s_attn = nn.Linear(N_EMBED, 3 * N_EMBED, bias=False) 
-        self.register_buffer(
-            "tril",
-            torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)).view(
-                1, 1, BLOCK_SIZE, BLOCK_SIZE
-            ),
-        )
+        self.register_buffer("tril", torch.tril(torch.ones(BLOCK_SIZE, BLOCK_SIZE)).view( 1, 1, BLOCK_SIZE, BLOCK_SIZE))
         self.n_head = num_heads
         self.proj = nn.Linear(N_EMBED, N_EMBED, bias=False)
         self.attn_dropout = nn.Dropout(DROPOUT)
@@ -75,18 +66,35 @@ class MultipleHeadSelfAttention(nn.Module):
         head_dim = N_EMBED // num_heads
         self.register_buffer("rope_embeds", precompute_rope_embeddings(head_dim, BLOCK_SIZE),persistent=False)
 
-    def forward(self, x):
+    def forward(self, x, past_kv = None):
         B, T, C = x.shape #BATCH_SIZE, BLOCK_SIZE, N_EMBED | (during generation (1, 1, N_EMBED))
 
         q, k, v = self.s_attn(x).split(N_EMBED, dim=2) #(B,T,C) * (C, 3*C) -> (B,T,3*C) -> (B,T,C) , (B,T,C) , (B,T,C) | (during generation, (1,1,C) * (C, 3*C) -> (1,1,3*C) -> (1,1,C), (1,1,C), (1,1,C))
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(
-            1, 2
-        )  # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, T, nh, hs) -> (B, nh, T, hs) | (1, 1, nh, hs) -> (1, nh, 1, hs)
 
-        q = apply_rotary_pos_emb(q, self.rope_embeds)
-        k = apply_rotary_pos_emb(k, self.rope_embeds)
+        if past_kv is None:
+            past_len = 0
+        else:
+            past_k, past_v = past_kv
+            past_len = past_k.shape[-2]
+
+        max_offset = BLOCK_SIZE - T
+        if past_len > max_offset:
+            past_len = max_offset
+
+        q = apply_rotary_pos_emb(q, self.rope_embeds, offset=past_len)
+        k = apply_rotary_pos_emb(k, self.rope_embeds, offset=past_len)
+
+        if past_kv is not None:
+            k = torch.cat((past_k, k), dim=-2)
+            v = torch.cat((past_v, v), dim=-2)
+
+            if k.shape[-2] > BLOCK_SIZE:
+                k = k[:, :, -BLOCK_SIZE:, :]
+                v = v[:, :, -BLOCK_SIZE:, :]
+
 
         T_k = k.shape[-2]
 
@@ -100,7 +108,7 @@ class MultipleHeadSelfAttention(nn.Module):
 
         out = self.resi_dropout(self.proj(out))
 
-        return out
+        return out, (k,v)
 
 
 class FeedForward(nn.Module):
@@ -125,10 +133,11 @@ class Block(nn.Module):
         self.ln1 = nn.LayerNorm(N_EMBED)
         self.ln2 = nn.LayerNorm(N_EMBED)
 
-    def forward(self, x):
-        x = x + self.sa(self.ln1(x)) #residual path + layer-norm before attention block
+    def forward(self, x, past_kv = None):
+        attn_out, past_kv = self.sa(self.ln1(x), past_kv) #residual path + layer-norm before attention block
+        x = x + attn_out
         x = x + self.ffwd(self.ln2(x)) #residual path + layer norm before feed forward network
-        return x    
+        return x,past_kv    
 
 
 class GPT(nn.Module):
@@ -136,16 +145,24 @@ class GPT(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(VOCAB_SIZE, N_EMBED) #Each unique token will have an embedding of size N_EMBED
         # self.position_embedding_table = nn.Embedding(BLOCK_SIZE, N_EMBED) #Similarly each position in a block, will have an embedding vector of size N_EMBED 
-        self.block = nn.Sequential(*[Block(n_head=N_HEAD) for _ in range(N_LAYER)]) #* is used to convert list of block to positional arguments.
+        # self.block = nn.Sequential(*[Block(n_head=N_HEAD) for _ in range(N_LAYER)]) #* is used to convert list of block to positional arguments.
+        self.blocks = nn.ModuleList([Block(n_head=N_HEAD) for _ in range(N_LAYER)])
         self.lm_head = nn.Linear(N_EMBED, VOCAB_SIZE) #(BLOCK_SIZE * N_EMBED) * (N_EMBED * VOCAB_SIZE)
 
-    def forward(self, idx, targets=None):
+    def forward(self, idx, targets=None, kv_cache = None):
         B, T = idx.shape #BATCH_SIZE, BLOCK_SIZE. (inputs)
 
         tok_emb = self.token_embedding_table(idx) #(B,T,C) where C is N_EMBED
         # pos_emb = self.position_embedding_table(torch.arange(T, device=DEVICE)) #(T,C) matrix
         x = tok_emb#(B,T,C) + (T,C) (broadcasting happens here)
-        x = self.block(x) #Through Transformer (B, T, C) -> (B, T, C)
+        #x = self.block(x) #Through Transformer (B, T, C) -> (B, T, C)
+        if kv_cache is None:
+            for block in self.blocks:
+                x,_ = block(x)
+        else:
+            for i, block in enumerate(self.blocks):
+                x, kv_cache[i] = block(x,kv_cache[i])
+        
         logits = self.lm_head(x) #Language Model head (B, T, C) -> (B, T, VOCAB_SIZE)
 
         if targets is not None:
@@ -156,18 +173,25 @@ class GPT(nn.Module):
         else:
             loss = None
 
-        return logits, loss
+        return logits, loss, kv_cache
 
     def generate(self, idx, max_new_tokens):
-        for _ in range(max_new_tokens):
-            idx_cond = idx[:, -BLOCK_SIZE:]
+        
+        kv_cache = [None for _ in range(N_LAYER)]
 
-            logits, _ = self(idx_cond)
+        logits,_,kv_cache = self(idx, kv_cache = kv_cache) #prefilling the cache
+        for _ in range(max_new_tokens):
+
+            idx_cond = idx[:, -1:]
+
+            logits,_,kv_cache = self(idx_cond, kv_cache = kv_cache)
             logits = logits[:, -1, :]
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
+
+
 
 model = GPT()
 model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
@@ -177,7 +201,9 @@ model.eval()
 context = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
 
 time_start = timer()
-model_output = tokenizer.decode(model.generate(context, max_new_tokens=MAX_NEW_TOKENS)[0].tolist())
+with torch.no_grad():
+    model_output = tokenizer.decode(model.generate(context, max_new_tokens=MAX_NEW_TOKENS)[0].tolist())
+
 time_end = timer()
 
 with open(FILE_PATH, "w") as file:
